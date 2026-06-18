03e5476a088721dea830e4d4fc269bb766dbc1c3
KarenMartinez <k.nathalia.martinez.b@gmail.com>
2026-06-18 12:25:08 -0500

    cambios nav

--- index-scripts/colorNavbar.js
+++ index-scripts/colorNavbar.js
@@ -1,12 +1,22 @@
+function cargarComponente(id, archivo, callback) {
+  fetch(archivo)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(id).innerHTML = data;
 
-
-const title = document.getElementsByTagName('title')[0].textContent;
-
-const pageList = document.getElementById('ul-navbar').children;
-
-for (let page of pageList) {
-    if (page.innerText === title) {
-        page.setAttribute('style', 'text-shadow: 0 4px 8px #3861FC;');
-    }
+      if (callback) {
+        callback();
+      }
+    })
+    .catch(error => {
+      console.error("Error cargando el componente:", error);
+    });
 }
 
+cargarComponente("navbar", "componentes/navbar.html", function () {
+  const script = document.createElement("script");
+  script.src = "index-scripts/colorNavbar.js";
+  document.body.appendChild(script);
+});
+
+cargarComponente("footer", "componentes/footer.html");
