3c5e874f57cc878a1da3a1eb1f07fdb33b546702
Juan Ruiz <juanruizdevia@gmail.com>
2026-06-18 01:57:17 -0500

    index´s navbar has been created

---
+++ 25.3SobreNosotro/scripts/colorNavbar.js
@@ -0,0 +1,12 @@
+
+export const changeColorNavbar = function () {
+    const title = document.getElementsByTagName('title')[0].textContent;
+
+    const pageList = document.getElementById('ul-navbar').children;
+
+    for (let page of pageList) {
+        if (page.innerText === title) {
+            page.setAttribute('style', 'text-shadow: 0 4px 8px #3861FC;');
+        }
+    }
+}
