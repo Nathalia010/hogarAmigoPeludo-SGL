ec10a42a15b8894add24a420c355d111f2366b93
Juan Ruiz <juanruizdevia@gmail.com>
2026-06-23 12:38:08 -0500

    pull from remote

--- components/navbar-footer/scripts/navbar.js
+++ components/navbar-footer/scripts/navbar.js
@@ -2,8 +2,16 @@
 export class navbar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <nav>
-                <p>logo aqui</p>
+            <nav class="custom-navbar">
+                <p class="navbar-logo">logo aqui</p>
+
+                <!-- Controladores exclusivos para el comportamiento responsive -->
+                <input type="checkbox" id="navbar-menu-toggle" class="navbar-hidden-checkbox">
+                <label for="navbar-menu-toggle" class="navbar-hamburger-icon">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
 
                 <ul id="ul-navbar">
                     <li><a href="#">Inicio</a> </li>
@@ -13,7 +21,6 @@ export class navbar extends HTMLElement {
                     <li><a href="#">Contactenos</a></li>
                     <li><a href="#">login</a> </li>
                 </ul>
-                
             </nav>`
     }
 
