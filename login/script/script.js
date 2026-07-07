58ada7d16fb74b6fbee58d3248c6b9e2dc6e9823
Stefany <stefanuis09@gmail.com>
2026-07-07 12:58:02 -0500

    Add files via upload

---
+++ login/script/script.js
@@ -0,0 +1,51 @@
+
+
+if (!localStorage.getItem("usuarios")) {
+
+    const usuarios = [
+        {
+            email: "admin@hogaramigo.com",
+            password: "123456",
+            nombre: "Administrador"
+        }
+    ];
+
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+}
+
+const btnLogin = document.getElementById("btnLogin");
+
+btnLogin.addEventListener("click", function () {
+
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    if (email === "" || password === "") {
+        alert("Completa todos los campos.");
+        return;
+    }
+
+    const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+
+    const usuario = usuarios.find(u =>
+        u.email === email &&
+        u.password === password
+    );
+
+    if (usuario) {
+
+        //Guardar la sesión
+        localStorage.setItem("usuarioLogueado", JSON.stringify(usuario));
+
+        alert("Bienvenido " + usuario.nombre);
+
+        //Redireccionar
+        window.location.href = "inicio.html";
+
+    } else {
+
+        alert("Correo o contraseña incorrectos.");
+
+    }
+
+});
